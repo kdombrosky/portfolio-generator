51f8681b04fe4c3deee66795e34a84526587261c
Kayla Dombrosky <kdombroskyy@live.com>
2021-06-16 20:38:03 -0400

    add required validation to profile prompts

--- app.js
+++ app.js
@@ -73,17 +73,41 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required):',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('Please enter some details about yourself.');
+                    return false;
+                }
+            }
         }
     ]);
 };
